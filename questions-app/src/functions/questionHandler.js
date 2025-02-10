53fe02864f64c9a87f0980bc84fbae2a70edfba0
DOUGLAS JOSUE CASCO HERRERA <djcasco@unah.hn>
2025-02-10 01:24:29 -0600

    Agregando Preguntas frecuentes version 1

--- questions-app/src/functions/questionHandler.js
+++ questions-app/src/functions/questionHandler.js
@@ -1,6 +1,24 @@
-// handlers/preguntasFrecuentesHandler.js
 const { getPreguntasFrecuentesController } = require('../controllers/PreguntasFrecuentesController');
 
+const headersACL = {
+    'Access-Control-Allow-Origin': '*', // Permitir todas las solicitudes
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+};
+
 module.exports.getPreguntasFrecuentes = async (event) => {
-    return await getPreguntasFrecuentesController(event);
+    try {
+        const preguntas = await getPreguntasFrecuentesController(event);
+        return {
+            statusCode: 200,
+            headers: headersACL,
+            body: JSON.stringify(preguntas),
+        };
+    } catch (error) {
+        return {
+            statusCode: 401,
+            headers: headersACL,
+            body: JSON.stringify({ message: error.message }),
+        };
+    }
 };
