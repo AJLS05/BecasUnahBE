3d7e98c3681797d79df5dc3349b1a3ab1f1652e2
DOUGLAS JOSUE CASCO HERRERA <djcasco@unah.hn>
2025-02-09 18:45:57 -0600

    Agregando validacion de empleado a login

--- Plataforma-APP/src/functions/loginHandler.js
+++ Plataforma-APP/src/functions/loginHandler.js
@@ -1,5 +1,11 @@
 const userService = require('../services/loginService');
 
+const headersACL ={
+    "Access-Control-Allow-Origin": "*",  // Permitir solicitudes de cualquier origen
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+}
+
 module.exports.login = async (event) => {
     const { no_cuenta, contrasena } = JSON.parse(event.body);
 
@@ -7,21 +13,13 @@ module.exports.login = async (event) => {
         const user = await userService.validateUser(no_cuenta, contrasena);
         return {
             statusCode: 200,
-            headers: {
-                "Access-Control-Allow-Origin": "*",  // Permitir solicitudes de cualquier origen
-                "Access-Control-Allow-Methods": "POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type",
-            },
+            headers: headersACL,
             body: JSON.stringify(user), // Devuelves el usuario autenticado
         };
     } catch (error) {
         return {
             statusCode: 401,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type",
-            },
+            headers: headersACL,
             body: JSON.stringify({ message: error.message }), // En caso de error
         };
     }
--- Plataforma-APP/src/repositories/userRepository.js
+++ Plataforma-APP/src/repositories/userRepository.js
@@ -1,32 +1,57 @@
-// src/repositories/userRepository.js
-const  sequelize  = require('../config/conexion');
+const sequelize = require('../config/conexion');
 const { QueryTypes } = require('sequelize');
 
 class UserRepository {
-    // Método para obtener el usuario por no_cuenta y contraseña
-    async getUserByUsernameAndPassword(no_cuenta, contrasena) {
+    async getUserByUsernameAndPassword(credencialNumerica, contrasena) {
         try {
-          if (!sequelize) throw new Error('Sequelize no está inicializado');
-          
-          const result = await sequelize.query(
-            'SELECT * FROM becario WHERE no_cuenta = :no_cuenta AND contrasena = :contrasena',
-            { 
-              replacements: { no_cuenta,contrasena },
-              type: QueryTypes.SELECT 
+            if (!sequelize) throw new Error('Sequelize no está inicializado');
+
+            // Buscar en la tabla becario
+            let result = await sequelize.query(
+                `SELECT becario.*, persona.primer_ingreso 
+                  FROM 
+                      becario
+                  LEFT JOIN 
+                      persona ON becario.persona_id = persona.persona_id
+                  WHERE 
+                  becario.no_cuenta = :credencialNumerica;`,
+                {
+                    replacements: { credencialNumerica },
+                    type: QueryTypes.SELECT
+                }
+            );
+
+            // Si no hay coincidencias en becario, buscar en empleado
+            if (result.length === 0) {
+                result = await sequelize.query(
+                    `SELECT empleado.*, persona.primer_ingreso 
+                  FROM 
+                      empleado
+                  LEFT JOIN 
+                      persona ON empleado.persona_id = persona.persona_id
+                  WHERE 
+                  empleado.no_empleado = :credencialNumerica;`,
+                    {
+                        replacements: { credencialNumerica },
+                        type: QueryTypes.SELECT
+                    }
+                );
             }
-          );
-      
-          if (result.length === 0) return null;
-          
-          const user = result[0];
-          if (contrasena !== user.contrasena) return null;
-          
-          return user;
+
+            // Si sigue sin encontrar resultados, retornar null
+            if (result.length === 0) return null;
+
+            const user = result[0];
+
+            // Verificar la contraseña
+            if (contrasena !== user.contrasena) return null;
+
+            return user;
         } catch (error) {
-          console.error('Error en getUserByUsernameAndPassword:', error);
-          throw error; // Propaga el error para manejarlo en el controlador
+            console.error('Error en getUserByUsernameAndPassword:', error);
+            throw error; // Propagar el error
         }
-      }
+    }
 }
 
 module.exports = new UserRepository();
